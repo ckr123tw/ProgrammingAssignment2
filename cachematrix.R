ca5a6311a9ff8d8ee8ef5a2abb18852b8d90fa48
ckr123tw <ckr123@gmail.com>
2015-05-17 19:51:30 -0500

    Revert "Branch1"

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,15 @@
-##  Functions for caching the inverse of matrices
+## Put comments here that give an overall description of what your
+## functions do
 
-## Create and set inverse of matrix
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinv <- function(inverse) inv <<- inverse
-  getinv <- function() inv
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+
 }
 
-## Fetch cached inverse matrix if it exists. Otherwise, compute the inverse.
+
+## Write a short comment describing this function
+
 cacheSolve <- function(x, ...) {
-  inv <- x$getinv()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinv(inv)
-  inv
+        ## Return a matrix that is the inverse of 'x'
 }
